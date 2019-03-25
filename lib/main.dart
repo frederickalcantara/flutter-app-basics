2c63a5634747451ed0167d2b8dded7fda74008f3
frederickalcantara <frederickaalcantara@gmail.com>
2019-03-25 19:42:01 -0400

    Added the ability to change the theme navbar color

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Startup Name Generator', home: RandomWords());
+    return new MaterialApp(
+      title: 'Startup Name Generator',
+      theme: new ThemeData(
+        primaryColor: Colors.blue[800],
+      ),
+      home: new RandomWords(),
+    );
   }
 }
 
